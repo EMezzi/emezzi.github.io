7ba425db58a9fb7c724bf8f0257d00d1eaf5e486
EMezzi <mezziemanuele@outlook.com>
2026-08-16 12:55:26 +0200

    About card commit.

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,37 +8,36 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi everyone! I’m <span className="purple">Soumyajit Behera</span>{" "}
-            from <span className="purple">Bhubaneswar, India</span>.
+            Hi everyone! I’m <span className="purple">Emanuele Mezzi</span>{" "}
+            from <span className="purple">Italy</span>.
             <br />
             I’m currently working as a{" "}
-            <span className="purple">Software Developer</span> at{" "}
-            <span className="purple">Juspay</span>.
-            <br />I hold an Integrated M.Sc. (IMSc) in{" "}
-            <span className="purple">Mathematics and Computing</span> from{" "}
-            <span className="purple">BIT Mesra</span>.
+            <span className="purple">PhD Candidate</span> at{" "}
+            <span className="purple">Vrije Universiteit Amsterdam</span>.
+            <br />I hold an MSc in{" "}
+            <span className="purple">Data Science</span> from{" "}
+            <span className="purple">University of Amsterdam</span>.
             <br />
             <br />
-            Outside of coding, I love engaging in activities that keep me
-            creative and inspired:
+            Other than researching Neurosymbolic AI in security, I love reading, traveling, and cooking:
           </p>
 
-          <ul>
-            <li className="about-activity">
-              <ImPointRight /> Playing Games 🎮
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs ✍️
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Traveling and Exploring New Places 🌍
-            </li>
-          </ul>
+          {/*<ul>*/}
+          {/*  <li className="about-activity">*/}
+          {/*    <ImPointRight /> Playing Games 🎮*/}
+          {/*  </li>*/}
+          {/*  <li className="about-activity">*/}
+          {/*    <ImPointRight /> Writing Tech Blogs ✍️*/}
+          {/*  </li>*/}
+          {/*  <li className="about-activity">*/}
+          {/*    <ImPointRight /> Traveling and Exploring New Places 🌍*/}
+          {/*  </li>*/}
+          {/*</ul>*/}
 
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Emanuele</footer>
         </blockquote>
       </Card.Body>
     </Card>
